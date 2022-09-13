11f42dd26b76524d83659bf7270850148002e58a
Devello-95 <15765156+Devello-95@users.noreply.github.com>
2022-09-13 15:40:29 +0530

    updated

--- src/main/java/com/carbooking/SwaggerConfig.java
+++ src/main/java/com/carbooking/SwaggerConfig.java
@@ -26,13 +26,13 @@ public class SwaggerConfig {
     }
     private ApiInfo metaData() {
         ApiInfo apiInfo = new ApiInfo(
-                "Sample Car Booking App",
-                "REST API for Car Booking",
+                "Go Cheeta",
+                "REST API for Go Cheeta",
                 "1.0",
                 "Terms of service",
-                new Contact("Harry Potter", "http://samplecarbooking.com/about/", "harry@scb.com"),
+                new Contact("Uditha Adhikary", "http://gocheeta.com/about/", "chathurangaadhikary@gmail.com"),
                "Test License 2.0",
-                "http://www.testlicense.com/licenses/TEST-LICENSE-2.0");
+                "http://www.gocheeta.com/licenses/TEST-LICENSE-2.0");
         return apiInfo;
     }
 }
